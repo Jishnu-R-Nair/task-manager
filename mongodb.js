d98ee201f76fecb823874dc13d4a677b5952b34d
Jishnu R Nair <rnair.jishnu@gmail.com>
2021-02-08 21:07:46 +0530

    added initial files

---
+++ mongodb.js
@@ -0,0 +1,24 @@
+// CRUD
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) return console.log('Unable to connect to database');
+
+    console.log('Connected to database');
+
+    const db = client.db(databaseName);
+
+    db.collection('users').insertOne({
+      name: 'Jishnu',
+      age: 27,
+    });
+  }
+);

6b15efd55ae89fadde925887a1656e05c0ca1cca
Jishnu R Nair <rnair.jishnu@gmail.com>
2021-02-09 13:01:49 +0530

    brush up CRUD using mongo

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,9 @@
 // CRUD
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
@@ -16,9 +18,64 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-      name: 'Jishnu',
-      age: 27,
-    });
+    // db.collection('users').findOne(
+    //   { _id: new ObjectID('60215972f51cd96091167762') },
+    //   (error, user) => {
+    //     if (error) return console.log('Unable to find one');
+
+    //     console.log(user);
+    //   }
+    // );
+
+    // db.collection('users')
+    //   .find({ age: 27 })
+    //   .toArray((error, users) => {
+    //     console.log('users', users);
+    //   });
+
+    // db.collection('users')
+    //   .find({ age: 27 })
+    //   .count((error, count) => {
+    //     console.log('users count', count);
+    //   });
+
+    // db.collection('tasks').findOne(
+    //   { _id: new ObjectID('602177c3038eaf63fce59d1e') },
+    //   (error, task) => {
+    //     console.log('task by id', task);
+    //   }
+    // );
+
+    // db.collection('tasks')
+    //   .find({ completed: false })
+    //   .toArray((error, tasks) => {
+    //     console.log('tasks', tasks);
+    //   });
+
+    // const updatePromise = db
+    //   .collection('users')
+    //   .updateOne(
+    //     { _id: new ObjectID('60215972f51cd96091167762') },
+    //     { $inc: { age: 1 } }
+    //   );
+
+    // updatePromise
+    //   .then((result) => console.log(result))
+    //   .catch((error) => console.log(error));
+
+    // db.collection('tasks')
+    //   .updateMany({ completed: false }, { $set: { completed: true } })
+    //   .then((result) => console.log(result))
+    //   .catch((error) => console.log(error));
+
+    // db.collection('users')
+    //   .deleteMany({ age: 26 })
+    //   .then((result) => console.log(result))
+    //   .catch((error) => console.log(error));
+
+    // db.collection('tasks')
+    //   .deleteOne({ description: 'kjdhfkjdhfkjhdfkj' })
+    //   .then((result) => console.log(result))
+    //   .catch((error) => console.log(error));
   }
 );

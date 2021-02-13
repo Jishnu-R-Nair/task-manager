9ef108490248ca425071d483ffeeec4554a49cbb
Jishnu R Nair <rnair.jishnu@gmail.com>
2021-02-13 19:24:31 +0530

    update apis for task and user

--- src/index.js
+++ src/index.js
@@ -42,6 +42,29 @@ app.get('/users/:id', async (req, res) => {
   }
 });
 
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'age', 'password'];
+  const isValidUpdateOperation = updates.every((u) =>
+    allowedUpdates.includes(u)
+  );
+
+  if (!isValidUpdateOperation)
+    return res.status(400).send({ error: 'Invalid updates!' });
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) return res.status(404).send();
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
 
@@ -72,6 +95,32 @@ app.get('/tasks/:id', async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).send();
+  }
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body || {});
+  const allowedUpdates = ['description', 'completed'];
+
+  const isValidUpdateOperation = updates.every((u) =>
+    allowedUpdates.includes(u)
+  );
+
+  if (!isValidUpdateOperation)
+    return res.status(400).send({ error: 'Invalid updates!' });
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) return res.status(404).send();
+
+    res.send(task);
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e);
   }
 });
 
